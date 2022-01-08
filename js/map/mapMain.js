3da20c04d718542e41b7833ece8f8d28582ef5df
DESKTOP-CMKFK60\g2030 <decievemyself@gmail.com>
2022-01-08 19:57:34 +0800

    dev1.5

--- js/map/mapMain.js
+++ js/map/mapMain.js
@@ -1,26 +1,4 @@
 
-     //defs //定義proj4套件協助座標轉換
-     proj4.defs([
-      [
-          'EPSG:4326',
-          '+title=WGS84 (long/lat) +proj=longlat +ellps=WGS84 +datum=WGS84 +units=degrees'],
-      [
-          'EPSG:3826',
-          '+title=TWD97 TM2 +proj=tmerc +lat_0=0 +lon_0=121 +k=0.9999 +x_0=250000 +y_0=0 +ellps=GRS80 +units=m +no_defs'
-      ],
-      [
-          'EPSG:3828',
-          '+title=TWD67 TM2 +proj=tmerc +lat_0=0 +lon_0=121 +k=0.9999 +x_0=250000 +y_0=0 +ellps=aust_SA +towgs84=-752,-358,-179,-0.0000011698,0.0000018398,0.0000009822,0.00002329 +units=m +no_defs'
-      ]
-    ]);
-    
-
-  //EPSG
-  var EPSG3826 = new proj4.Proj('EPSG:3826'); //TWD97 TM2(121分帶)
-  var EPSG3828 = new proj4.Proj('EPSG:3828'); //TWD67 TM2(121分帶)
-  var EPSG4326 = new proj4.Proj('EPSG:4326'); //WGS84
-
-
 var mapConfig = {
     entity:{},  
     clickStatus : "default",
@@ -31,9 +9,11 @@ var mapMain = {
     data:{
         centerLongitudeLatitude:ol.proj.fromLonLat([120.2278, 22.5243]),   
         featureLayerArrCenter:[], 
-        featureLayerArrBuffer : [],
-        calHtml: null,
-        calCount : 0
+        featureLayerArrBuffer : [],    
+        queryShipOBj:{  //查詢半徑內漁船參數
+          radius:0, //公尺
+          circleCenter_3857:null,
+        }
     },
     methods: {
         initMap:function(){
@@ -55,12 +35,12 @@ var mapMain = {
                 if(mapConfig.clickStatus === "mapBuffer"){
                   if(document.getElementById('seaMile').value === ""){
                     alert('請輸入半徑(單位:海哩)');
-                    return
+                    return;
                   }       
 
                   if(isNaN(document.getElementById('seaMile').value)){
                     alert('請輸入數字');
-                    return
+                    return;
                   }
                   
                   mapMain.methods.addfeatrueLayer(evt.coordinate);
@@ -87,58 +67,26 @@ var mapMain = {
             mapConfig.entity.olMap.removeLayer(mapConfig.entity.featureLayer_buffer);
           }
 
+
           //恢復查詢初始狀態
-          mapMain.data.calCount = 0;
-          if(mapMain.data.calHtml)
-          clearInterval(mapMain.data.calHtml);
-     
+          // mapMain.data.calCount = 0;
+          // if(mapMain.data.calHtml)
+          // clearInterval(mapMain.data.calHtml);
+          //document.getElementById('seaMile').value = "";
+
           document.getElementsByClassName('input-ui-res')[0].innerHTML = "查詢結果：";
         },
         addfeatrueLayer:function(coordinateArr){
-          //3857 --> 4326
-          var coords_4326Arr = ol.proj.transform(coordinateArr,"EPSG:3857","EPSG:4326");
-           //4326 -->3826(WGS84經緯度轉TWD97 TM2)
-          var coords_3826Arr = proj4(EPSG4326, EPSG3826, coords_4326Arr);                    
-
+                           
           mapMain.methods.clearMapBufferLayer();
           //換算為公尺  1海哩 = 1852公尺
           var seaMile = parseFloat(document.getElementById('seaMile').value) * 1852;
 
-          //製作3826矩形
-         var Rectangle_3826 = {
-          maxX:coords_3826Arr[0] + seaMile,        
-          maxY:coords_3826Arr[1] + seaMile,
-          minX:coords_3826Arr[0] - seaMile,
-          minY:coords_3826Arr[1] - seaMile
-         }; 
-         
-         var Max4326Arr = proj4(EPSG3826, EPSG4326, [Rectangle_3826.maxX, Rectangle_3826.maxY]);
-         //console.log(a);
-         var Min4326Arr = proj4(EPSG3826, EPSG4326, [Rectangle_3826.minX, Rectangle_3826.minY]);
-         //console.log(b);
-         //製作4326矩形
-         var bufferRectangle_4326 = {
-          maxX:Max4326Arr[0],
-          maxY:Max4326Arr[1],
-          minX:Min4326Arr[0],        
-          minY:Min4326Arr[1]
-         }
-         
-         mapMain.data.calCount = 0;
-         mapMain.data.calHtml = setInterval(function(){
-          if( mapMain.data.calCount % 5 === 0){
-            document.getElementsByClassName('input-ui-res')[0].innerHTML = "計算中.";
-          }else if( mapMain.data.calCount % 5 === 1){
-            document.getElementsByClassName('input-ui-res')[0].innerHTML = "計算中..";
-          }else if( mapMain.data.calCount % 5 === 2){
-            document.getElementsByClassName('input-ui-res')[0].innerHTML = "計算中...";
-          }else if( mapMain.data.calCount % 5 === 3){
-            document.getElementsByClassName('input-ui-res')[0].innerHTML = "計算中...";
-          }else if( mapMain.data.calCount % 5 === 4){
-            document.getElementsByClassName('input-ui-res')[0].innerHTML = "計算中...";
-          }
-          mapMain.data.calCount++;
-         }, 300)
+          //將半徑寫入全域
+          mapMain.data.queryShipOBj.radius = seaMile;
+          //將地圖點擊的中心點寫入全域
+          mapMain.data.queryShipOBj.circleCenter_3857 = coordinateArr;
+        
          //console.log(bufferRectangle_4326);
 
           mapMain.data.featureLayerArrCenter = [];
@@ -194,4 +142,3 @@ var mapMain = {
 };
 
 mapMain.methods.initMap();
-//mapMain.methods.addfeatrueLayer();
